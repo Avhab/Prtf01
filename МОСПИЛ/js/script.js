af17fcd4792979baf82e2f1c638042ae1a7bc1e5
Аркадий <101921286+Avhab@users.noreply.github.com>
2022-06-04 00:26:21 +0700

    Add files via upload

--- МОСПИЛ/js/script.js
+++ МОСПИЛ/js/script.js
@@ -4,15 +4,32 @@ for (let i = 0; i < HorScrl.length; i++) {
 	let butnR = HorScrl[i].querySelectorAll(".butnR");butnR=butnR[0];//кнопка переключения вправо
 	let ScrlCont = HorScrl[i].querySelectorAll(".ScrlCont");ScrlCont=ScrlCont[0];//контейнер элементов-скролла
 	let ScrlElem = HorScrl[i].querySelectorAll(".ScrlElem");//элементы переключения - слайды
-	let scrlStep = ScrlCont.scrollWidth/ScrlElem.length; //шаг скроллинга в пикселах
-			
+	let NmSld = ScrlElem.length;
+	let scrlStep = Math.trunc(ScrlCont.scrollWidth / NmSld); //шаг скроллинга в пикселах
+	let curSld = 0;
+	let ScrlTime;
+	let ScrlFlag = true;
+
 	if (butnL) {butnL.onclick = function(){
-		ScrlCont.scrollBy({left: -scrlStep, behavior: 'smooth'});
-		};}//кнопка переключения влево
+		curSld = curSld-1;
+		if (curSld<0) {curSld=0;};
+		ScrlCont.scrollTo({left: (scrlStep * curSld), behavior: 'smooth'});	}	}//кнопка переключения влево
 	if (butnR) {butnR.onclick = function(){
-		ScrlCont.scrollBy({left: scrlStep, behavior: 'smooth'});
-		};}//кнопка переключения вправо
-	}
+		if (scrlStep*(NmSld-curSld)>ScrlCont.clientWidth) {
+			if (curSld<(NmSld - 1)) {curSld = curSld+1;}
+			ScrlCont.scrollTo({left: (scrlStep * curSld), behavior: 'smooth'});	}	};	}//кнопка переключения вправо
+	function AnScroll() {	//Обработка скролла
+		if (ScrlFlag) {//выполняется, в состоянии движения скролла по инерции
+			clearTimeout(ScrlTime);
+			ScrlTime = setTimeout( function() {
+				if (ScrlCont.scrollLeft+ScrlCont.clientWidth+5<=ScrlCont.scrollWidth) {
+					let tmp=Math.trunc((ScrlCont.scrollLeft/scrlStep)+0.5);
+					curSld = tmp;
+					ScrlCont.scrollTo({left: (scrlStep * curSld), behavior: 'smooth'});	}	}, 100);}}
+	ScrlCont.addEventListener("scroll", function (e) { AnScroll(); }); //Обработка скролла
+	ScrlCont.addEventListener("touchstart", function (e) { ScrlFlag = false; }); //Начало касания
+	ScrlCont.addEventListener("touchend", function (e) { ScrlFlag = true; AnScroll(); });//Пользователь отпустил экран
+}
 	
 //бургер меню
 let wideMenu = document.querySelectorAll(".wideMenu");wideMenu=wideMenu[0];
@@ -42,7 +59,6 @@ burgMenu.onclick = function(){
 	}, 300);
 	}
 //бургер меню-----end
-
 let header = document.querySelectorAll("header");header=header[0];
 let ordBut = header.querySelectorAll(".ordBut");ordBut=ordBut[0];
 let order = document.querySelectorAll(".order");order=order[0];
@@ -57,29 +73,11 @@ ordBut.onclick = function(){
 		order.style.display='inline-block';
 		document.location.href ="#order";
 		setTimeout( function() {
-			order.style.transform = 'scaleY(100%)';
-		}, 10);
-	}, 10);
-}
+			order.style.transform = 'scaleY(100%)';	}, 10);	}, 10);}
 ClsCrs.onclick = function(){
 	order.style.transform = 'scaleY(0)';
 	setTimeout( function() {order.style.display=null;}, 300);}
-/*
-SubmBut.onclick  = function(){
-	order.style.transform = 'scaleY(0)';
-	setTimeout( function() {
-		order.style.display=null;
-		formOrd.submit();
-	}, 300);
-}
-*/
-
 formOrd.onsubmit = function(formOrd){
-/*	formOrd.preventDefault();*/
 	order.style.transform = 'scaleY(0)';
 	setTimeout( function() {
-		order.style.display=null;
-/*		formOrd.submit();*/
-	}, 300);
-}
-
+		order.style.display=null;}, 300);}
