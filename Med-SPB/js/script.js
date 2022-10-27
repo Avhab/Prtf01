248e6689731c1496e15c58743656cd24a7e20514
Александр <101921286+Avhab@users.noreply.github.com>
2022-10-27 16:20:34 +0700

    Add files via upload

---
+++ Med-SPB/js/script.js
@@ -0,0 +1,310 @@
+let bkgr = document.querySelector("header");
+//let bkgr = document.querySelector("body");
+//let script = document.querySelector("script");
+let decor01 = document.createElement("div");
+// 1 - 10
+/*
+bkgr.prepend(decor01); bkgr.append(decor01);
+script.before(decor01);
+*/
+
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#5531D2;height:668px;width:668px;left:-280px;top: -14px;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#E7CDFC;height:103px;width:103px;left:calc(50% + (1698px - 970px + 52px));top: -3px;opacity:30%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#5531D2;height:20px;width:20px;left:calc(50% + (1590px - 970px + 10px));top:211px;opacity:30%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#E7CDFC;height:103px;width:103px;left:calc(50% + (1558px - 970px + 52px));top:627px;opacity:30%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#E7CDFC;height:103px;width:103px;left:calc(50% + (1858px - 970px + 52px));top:580px;opacity:30%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#5531D2;height:60px;width:60px;left:calc(50% + (1752px - 970px + 30px));top:364px;opacity:30%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#5531D2;height:60px;width:60px;left:calc(50% + (1035px - 970px + 30px));top:358px;opacity:30%;`
+
+if (document.documentElement.clientWidth > 1100) {
+bkgr = document.querySelector(".comprCode");
+//bkgr = document.querySelector("body");
+
+//8 - 11
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#5531D2;height:365px;width:365px;opacity:10%;left:-182px;top:150px;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #9E00FF;height:70px;width:254px;right:-50px;top:70px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #43CDD7;height:70px;width:254px;right:20px;top:314px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#5531D2;height:70px;width:254px;right:-40px;top:450px;transform:rotate(-15deg);`
+
+//11 - 13
+bkgr = document.querySelector(".ourWarr");
+/*
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:5;background:#F0F4FF;height:100px;width:270px;left:0;top:435px;`*/
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#C5A6FD;height:484px;width:484px;opacity:10%;left:-172px;top:229px;`
+
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#E7CDFC;height:70px;width:254px;right:-44px;top:175px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #E7CDFC;height:70px;width:254px;right:-104px;top:365px;transform:rotate(-15deg);`
+
+//14
+//bkgr = document.querySelector(".FAQ");
+bkgr = document.querySelector(".un01");
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#C5A6FD;height:563px;width:563px;opacity:10%;right:-260px;top:59px;`
+
+//15 - 20
+//bkgr = document.querySelector(".WhoN");
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#E7CDFC;height:70px;width:254px;left:-100px;top:480px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #43CDD7;height:70px;width:254px;left:-170px;top:710px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#5531D2;height:70px;width:254px;left:-40px;top:860px;transform:rotate(-15deg);`
+
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#E7CDFC;height:70px;width:254px;right:-50px;top:904px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#C5A6FD;height:70px;width:254px;right:-120px;top:1040px;transform:rotate(-15deg);`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #E7CDFC;height:70px;width:254px;right:-24px;top:1210px;transform:rotate(-15deg);`
+
+//21
+bkgr = document.querySelector(".Consult");
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#C5A6FD;height:132px;width:132px;opacity:100%;left:124px;top:20px;opacity:10%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#C5A6FD;height:94px;width:94px;opacity:100%;left:252px;top:420px;opacity:10%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #9E00FF;height:70px;width:254px;left:-54px;top:445px;transform:rotate(-15deg);opacity:40%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;border:4px solid #E7CDFC;height:70px;width:254px;left:-34px;top:245px;transform:rotate(-15deg);opacity:40%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#E7CDFC;height:70px;width:254px;right:-74px;top:285px;transform:rotate(-15deg);opacity:40%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:20px;background:#E7CDFC;height:70px;width:254px;right:-54px;top:485px;transform:rotate(-15deg);opacity:40%;`
+decor01 = document.createElement("div");
+bkgr.append(decor01);
+decor01.style.cssText=`position:absolute;z-index:-3;border-radius:500px;background:#9E00FF;height:176px;width:176px;opacity:30%;left:122px;top:620px;opacity:30%;`
+
+
+
+
+}
+
+
+
+let HScrol = document.querySelectorAll(".HScrol");
+if (HScrol){
+	for (let i = 0; i < HScrol.length; i++) {
+		if (document.documentElement.clientWidth < 1150) {
+			let scrlCont = HScrol[i].querySelector(".scrlCont");
+			let nStep = Math.round(scrlCont.scrollWidth/scrlCont.clientWidth);//кол-во шагов скролла
+			if (nStep>1) {
+				function swButt() { //переключение индикаторных кнопок - вызывается при скролле
+					let tmp = Math.round(scrlCont.scrollLeft/scrlCont.clientWidth);
+					let rBut = HScrol[i].querySelectorAll(".rButtCont>div");
+					for (let i = 0; i < rBut.length; i++) {
+						if (i==tmp) {rBut[i].className = 'On';}else{rBut[i].className = '';}	}	}
+				let nDiv = document.createElement("div"); //генерация индикаторных кнопок
+				nDiv.className = 'rButtCont';
+				scrlCont.after(nDiv);
+				let rButtCont = HScrol[i].querySelector(".rButtCont");
+				for (let j = 0; j < nStep; j++) {
+					nDiv = document.createElement("div");
+					rButtCont.append(nDiv);	}
+				scrlCont.addEventListener('scroll', function() { swButt();	});
+				swButt();	}	}	}	}
+
+
+
+//раскрывающиеся списки FAQ ------------
+
+let FAQ = document.querySelectorAll(".FAQ");
+for (let i = 0; i < FAQ.length; i++) {
+	let qAns = FAQ[i].querySelectorAll(".qAns");
+	for (let i = 0; i < qAns.length; i++) {
+		let Que = qAns[i].querySelector(".Que");
+		let Ans = qAns[i].querySelector(".Ans");
+		let wrapArr = document.createElement("div");
+		wrapArr.className = 'wrapArr';
+		qAns[i].append(wrapArr);
+		let heigOn = Ans.clientHeight + Ans.clientHeight*0.07;
+		Ans.style.height = "0";
+		Ans.style.opacity = '0';
+		Ans.style.padding = "0 17px";
+		
+		qAns[i].onclick = function() {
+			wrapArr.classList.toggle("ArrUp");
+			if (wrapArr.classList.contains("ArrUp")) {
+				Que.style.background = '#C5A6FD';
+				Que.style.borderRadius = '10px 10px 0 0';
+				Ans.style.padding = null;
+				Ans.style.height = heigOn + 'px';
+				Ans.style.opacity = '1';
+			} else {
+				Ans.style.height = "0";
+				Ans.style.padding = "0 17px";
+				Ans.style.opacity = '0';
+				Que.style.background = null
+				Que.style.borderRadius = null;	}	}	}	}
+//---------------раскрывающиеся списки FAQ
+
+//закрашивание звезд---------------
+let stars = document.querySelectorAll(".stars");
+for (let i = 0; i < stars.length; i++) {
+	let prnNod = stars[i].parentElement;
+	let score = prnNod.querySelector(".score");
+	score = (Number(score.innerHTML.replace(',','.')));
+	let intScore = Math.trunc(score);
+	let fraScore = score - intScore;
+	for (let j = 0; j < intScore; j++) {
+		let filStar = document.createElement("div");
+		stars[i].append(filStar);
+		filStar.style.cssText=`width:14px;`;	}
+	if (fraScore>0) {
+		let filStar = document.createElement("div");
+		stars[i].append(filStar);
+		fraScore = fraScore * 14;
+		filStar.style.cssText=`width:`+fraScore+`px;`;	}	}
+//---------------закрашивание звезд
+
+//копирование из хедера в футер---------------
+let tmp = document.querySelector("header .topMenu");
+let Cont2 = document.querySelector("footer .Cont2");
+if (tmp&&Cont2) {
+	tmp = tmp.cloneNode(true);
+	tmp.classList.remove("rowDiv");
+	Cont2.append(tmp);	}
+
+tmp = document.querySelector("header .logoDiv");
+let Cont1 = document.querySelector("footer .Cont1");
+if (tmp&&Cont1) {
+	tmp = tmp.cloneNode(true);
+	Cont1.append(tmp);}
+
+tmp = document.querySelector("header .midMenu>div:nth-child(3)>a");
+if (tmp&&Cont1) {
+	tmp = tmp.cloneNode(true);
+	Cont1.append(tmp);
+	tmp.style.marginTop="46px";	}
+
+tmp = document.querySelector("header .midMenu>div:nth-child(2)>div:nth-child(1)>a");
+if (tmp&&Cont1) {
+	tmp = tmp.cloneNode(true);
+	Cont1.append(tmp);
+	tmp.style.marginTop="8px";	}
+
+tmp = document.querySelector("header .midMenu>div:nth-child(2)>div:nth-child(2)>a");
+if (tmp&&Cont1) {
+	tmp = tmp.cloneNode(true);
+	Cont1.append(tmp);
+	tmp.style.marginTop="8px"; }
+//---------------копирование из хедера в футер
+
+//мобильное меню---------------
+if (document.documentElement.clientWidth < 600) {
+	let mobMenu = document.querySelector("header .mobMenu");
+	let burgerMenu = document.querySelector("header .burgerMenu");
+	let burger = mobMenu.querySelector(".rTopCor");
+	let cross = burgerMenu.querySelector(".rTopCor");
+
+	let topMenu = document.querySelector("header .topMenu");
+	let midMenu = document.querySelector("header .midMenu");
+	let bottomMenu = document.querySelector("header .bottomMenu");
+	let tmp = midMenu.querySelector(".logoDiv");
+	tmp = tmp.cloneNode(true);
+	mobMenu.prepend(tmp);
+	tmp = tmp.cloneNode(true);
+	burgerMenu.prepend(tmp);
+	mobMenu.style.display = 'inline-block';
+	topMenu.style.display = 'none';
+	midMenu.style.display = 'none';
+	bottomMenu.style.display = 'none';
+	
+	tmp = midMenu.querySelector(".midMenu>div:nth-child(2)>div:nth-child(1)");
+	tmp = tmp.cloneNode(true);
+	burgerMenu.append(tmp);
+	tmp = midMenu.querySelector(".midMenu>div:nth-child(2)>div:nth-child(2)");
+	tmp = tmp.cloneNode(true);	
+	burgerMenu.append(tmp);
+	tmp = midMenu.querySelector(".midMenu>div:nth-child(3)>*:nth-child(1)");
+	tmp = tmp.cloneNode(true);	
+	burgerMenu.append(tmp);
+	tmp = midMenu.querySelector(".midMenu>div:nth-child(3)>*:nth-child(2)");
+	tmp = tmp.cloneNode(true);	
+	burgerMenu.append(tmp);
+	let Cont2 = document.querySelector("footer .Cont2");
+	Cont2 = Cont2.cloneNode(true);	
+	burgerMenu.append(Cont2);
+	let Cont3 = document.querySelector("footer .Cont3");
+	Cont3 = Cont3.cloneNode(true);	
+	burgerMenu.append(Cont3);
+	tmp = midMenu.querySelector(".midMenu .paleBtn");
+	tmp = tmp.cloneNode(true);	
+	burgerMenu.append(tmp);
+
+
+	burger.onclick = function() {
+		burgerMenu.style.display = 'block';
+		mobMenu.style.display = 'none';
+	}
+	cross.onclick = function() {
+		burgerMenu.style.display = 'none';
+		mobMenu.style.display = 'inline-block';
+	}
+}
+
+
+//---------------мобильное меню
+
+/*
+ * 
+ * <div class="midMenu rowDiv">
+			<div>
+				<div class="logoDiv">
+					<a href="index.html"><img src="images/logo_icon.png" alt="Лого"></a>
+					<div>исследовательский центр новейших методов в наркологии</div>
+				</div>
+<header class="bluGrad">
+	<div class="wrap1">
+		<div class="topMenu rowDiv">
+			<div><a href="#">Лицензии</a></div>
+			<div><a href="#">Цены</a></div>
+			
+			
+footer .Cont1
+footer .Cont2
+footer .Cont3
+*/
+
