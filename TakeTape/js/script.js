6574e620d0039fbc62b041d997e65c1bdc94cf1f
Александр <101921286+Avhab@users.noreply.github.com>
2023-06-30 10:09:13 +0700

    Add files via upload

--- TakeTape/js/script.js
+++ TakeTape/js/script.js
@@ -57,12 +57,12 @@ for (let i = 0; i < phoneInput.length; i++) {
 //    console.log('length END: ' + phoneInput[i].value.length);
 window.addEventListener("DOMContentLoaded", function() {
     [].forEach.call( document.querySelectorAll('input[type="tel"]'), function(input) {
-    var keyCode;
+    let keyCode;
     function mask(event) {
         event.keyCode && (keyCode = event.keyCode);
-        var pos = this.selectionStart;
+        let pos = this.selectionStart;
         if (pos < 3) event.preventDefault();
-        var matrix = "+7 (___) ___ ____",
+        let matrix = "+7 (___) ___ ____",
             i = 0,
             def = matrix.replace(/\D/g, ""),
             val = this.value.replace(/\D/g, ""),
@@ -74,7 +74,7 @@ window.addEventListener("DOMContentLoaded", function() {
             i < 5 && (i = 3);
             new_value = new_value.slice(0, i)
         }
-        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        let reg = matrix.substr(0, this.value.length).replace(/_+/g,
             function(a) {
                 return "\\d{1," + a.length + "}"
             }).replace(/[+()]/g, "\\$&");
