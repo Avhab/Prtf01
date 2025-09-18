9ba55238069a3f970bd0616b5af34d81c6163500
Александр <101921286+Avhab@users.noreply.github.com>
2025-09-18 15:50:13 +0700

    Delete RuDIVE/header-butt-block.php

--- RuDIVE/header-butt-block.php
+++
@@ -1,4 +0,0 @@
-<div class="butCont hCont options">
-	<button class="constClr"><img src="images/user1.svg">Войти</button>
-	<button class="swThem constClr"><img src="images/sun1.svg"></button>
-</div>
